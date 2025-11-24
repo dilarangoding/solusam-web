62f9cfb5c29da52c57f1bd2abc7d91516e1e6075
Artemis <129715610+Sprougat@users.noreply.github.com>
2025-11-25 03:30:04 +0700

    Update Auth.php

--- app/Filters/Auth.php
+++ app/Filters/Auth.php
@@ -9,6 +9,14 @@ use CodeIgniter\HTTP\ResponseInterface;  // Interface untuk HTTP response
 class Auth implements FilterInterface
 {
     /**
+     ** Do whatever processing this filter needs to do.
+     * By default it should not return anything during
+     * normal execution. However, when an abnormal state
+     * is found, it should return an instance of
+     * CodeIgniter\HTTP\Response. If it does, script
+     * execution will end and that Response will be
+     * sent back to the client, allowing for error pages,
+     * redirects, etc.
      * Method ini dijalankan sebelum controller dieksekusi.
      * Gunanya untuk memeriksa apakah user sudah login.
      * Jika tidak, redirect ke halaman login.
@@ -29,10 +37,15 @@ class Auth implements FilterInterface
     /**
      * Method ini dijalankan setelah controller dieksekusi.
      * Bisa digunakan untuk memodifikasi response.
-     *
+     * Allows After filters to inspect and modify the response
+     * object as needed. This method does not allow any way
+     * to stop execution of other after filters, short of
+     * throwing an Exception or Error.
+     
      * @param RequestInterface $request
      * @param ResponseInterface $response
      * @param array|null $arguments
+     *
      * @return ResponseInterface|void
      */
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
