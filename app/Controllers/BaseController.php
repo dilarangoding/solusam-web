70fa803c98d26e4d2293fb6f15fc1d01a214979b
Febrianfalihalwafi <162521180+Febrianfalihalwafi@users.noreply.github.com>
2025-12-27 08:02:20 +0700

    Update BaseController.php

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,58 +1,95 @@
 <?php
 
+// Semua controller aplikasi berada di App\Controllers
 namespace App\Controllers;
 
+// Import class Controller utama dari CodeIgniter
 use CodeIgniter\Controller;
+// Import class Request khusus CLI (command line)
 use CodeIgniter\HTTP\CLIRequest;
+// Import class Request untuk HTTP (web request)
 use CodeIgniter\HTTP\IncomingRequest;
+// Import interface Request
 use CodeIgniter\HTTP\RequestInterface;
+// Import interface Response
 use CodeIgniter\HTTP\ResponseInterface;
+// Import Logger sesuai standar PSR (PHP Standard Recommendation)
 use Psr\Log\LoggerInterface;
 
 /**
  * Class BaseController
  *
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- * Extend this class in any new controllers:
- *     class Home extends BaseController
+ * BaseController adalah controller induk (parent controller)
+ * Semua controller lain DIANJURKAN mewarisi class ini.
  *
- * For security be sure to declare any new methods as protected or private.
+ * Tujuan:
+ * - Menyediakan tempat umum untuk inisialisasi
+ * - Menghindari duplikasi kode di setiap controller
+ *
+ * Contoh penggunaan:
+ * class Home extends BaseController
+ *
+ * Catatan keamanan:
+ * Method baru sebaiknya protected atau private
  */
 abstract class BaseController extends Controller
 {
     /**
-     * Instance of the main Request object.
+     * Instance utama Request
+     * Bisa berupa:
+     * - CLIRequest → jika dijalankan lewat terminal
+     * - IncomingRequest → jika lewat browser
      *
      * @var CLIRequest|IncomingRequest
      */
     protected $request;
 
-    /**
-     * An array of helpers to be loaded automatically upon
-     * class instantiation. These helpers will be available
-     * to all other controllers that extend BaseController.
+      /**
+     * Daftar helper yang otomatis dimuat
+     * saat controller dipanggil
+     *
+     * Helper adalah kumpulan fungsi siap pakai
+     * Contoh: form, url, text, dll
      *
      * @var list<string>
      */
     protected $helpers = [];
 
+     
     /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
+     * Catatan PHP 8.2:
+     * Properti dinamis tidak disarankan
+     *
+     * Jika ingin menggunakan session,
+     * sebaiknya dideklarasikan di sini
      */
     // protected $session;
 
     /**
+     * Method initController
+     *
+     * Method ini otomatis dipanggil SETIAP KALI
+     * controller dijalankan
+     *
+     * @param RequestInterface  $request  → data request dari user
+     * @param ResponseInterface $response → response ke browser
+     * @param LoggerInterface   $logger   → pencatatan log sistem
+     *
      * @return void
      */
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
-        // Do Not Edit This Line
+         // WAJIB dipanggil
+        // Menjalankan proses inisialisasi bawaan CI4
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
+       // Tempat untuk preload:
+        // - session
+        // - model
+        // - library
+        // - service lain
 
-        // E.g.: $this->session = service('session');
+        // Contoh jika ingin aktifkan session:
+        // $this->session = service('session');
     }
 }
