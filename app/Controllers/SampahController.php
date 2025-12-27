97bbb9086b37db2587a324c5712b849722084a4a
Febrianfalihalwafi <162521180+Febrianfalihalwafi@users.noreply.github.com>
2025-12-27 15:07:50 +0700

    Update SampahController.php

--- app/Controllers/SampahController.php
+++ app/Controllers/SampahController.php
@@ -4,27 +4,48 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
+/**
+ * SampahController
+ * Controller untuk mengelola data sampah (CRUD)
+ * Data sampah digunakan dalam transaksi pembelian dan penjualan
+ */
 class SampahController extends BaseController
-{
+{   
+    // Model untuk tabel data sampah
     protected $sampahModel;
+    // Model untuk tabel transaksi
     protected $transaksiModel;
 
+      /**
+     * Constructor
+     * Inisialisasi model yang digunakan
+     */
     public function __construct()
     {
         $this->sampahModel = new \App\Models\Sampah();
          $this->transaksiModel = new \App\Models\Transaksi();
     }
 
+    
+    /**
+     * Menampilkan daftar data sampah milik client yang sedang login
+     */
     public function index()
     {
         $data = [
             "title" => "Data Sampah",
-            "data" => $this->sampahModel->where('client_id', session()->get('clientId'))->findAll()
+             // Ambil data sampah berdasarkan client_id dari session
+            "data" => $this->sampahModel
+                ->where('client_id', session()->get('clientId'))
+                ->findAll()
         ];
 
         return view('sampah/index', $data);
     }
 
+    /**
+     * Menampilkan form tambah data sampah
+     */
     public function create()
     {
         $data = [
@@ -34,24 +55,37 @@ class SampahController extends BaseController
         return view('sampah/create', $data);
     }
 
+    
+    /**
+     * Menampilkan form edit data sampah
+     * @param int $id ID data sampah
+     */
     public function edit($id)
     {
         $data = [
             "title" => "Edit Data Sampah",
-            "data" => $this->sampahModel->where('id', $id)->first()
+            // Ambil data sampah berdasarkan ID
+            "data" => $this->sampahModel
+                ->where('id', $id)
+                ->first()
         ];
 
         return view('sampah/edit', $data);
     }
 
+     /**
+     * Menyimpan data sampah (insert atau update)
+     */
     public function store()
     {
+         // Ambil data dari form
         $nama_sampah = $this->request->getPost('nama_sampah');
         $harga_beli = $this->request->getPost('harga_beli');
         $harga_jual = $this->request->getPost('harga_jual');
         $satuan = $this->request->getPost('satuan');
         $id = $this->request->getPost('id');
 
+         // Data yang akan disimpan
         $data = [
             'nama_sampah' => $nama_sampah,
             'harga_beli' => $harga_beli,
@@ -59,16 +93,20 @@ class SampahController extends BaseController
             'satuan' => $satuan,
         ];
 
+        // Jika ada ID berarti update
         if ($id) {
             $data['id'] = $id;
             $text = 'diupdate';
+         // Jika tidak ada ID berarti insert data baru
         } else {
             $text = 'ditambahkan';
             $data['client_id'] = session('clientId');
         }
 
         try {
+             // Simpan data ke database
             $this->sampahModel->save($data);
+              // Pesan sukses
             $message = [
                 'title' => 'Success',
                 'text' => 'Data sampah berhasil ' . $text,
@@ -77,6 +115,7 @@ class SampahController extends BaseController
             session()->setFlashdata($message);
             return redirect()->to('sampah');
         } catch (\Throwable $th) {
+             // Pesan gagal
             $message = [
                 'title' => 'Error',
                 'text' => 'Data sampah gagal ' . $text,
@@ -87,8 +126,13 @@ class SampahController extends BaseController
         }
     }
 
+  /**
+     * Menghapus data sampah
+     * Dilarang menghapus jika masih memiliki transaksi penjualan
+     */
   public function delete()
 {
+    // Ambil ID sampah dari request
     $id = $this->request->getPost('id');
     
     try {
@@ -125,13 +169,15 @@ class SampahController extends BaseController
         }
         
     } catch (\Exception $e) {
+        // Jika terjadi error
         $response = [
             "title" => "Gagal", 
             "text" => "Data gagal dihapus: " . $e->getMessage(), 
             "icon" => "error"
         ];
     }
-    
+
+    // Kembalikan response JSON (biasanya dipakai AJAX)
     return $this->response->setJSON($response);
 }
 
