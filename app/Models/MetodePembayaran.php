3c517ccbafe54b108c022e7a7da197476a041199
Febrianfalihalwafi <162521180+Febrianfalihalwafi@users.noreply.github.com>
2025-12-27 15:13:08 +0700

    Update MetodePembayaran.php

--- app/Models/MetodePembayaran.php
+++ app/Models/MetodePembayaran.php
@@ -1,16 +1,33 @@
 <?php
 
+// Namespace model sesuai struktur CodeIgniter 4
 namespace App\Models;
 
+// Menggunakan Model bawaan CodeIgniter
 use CodeIgniter\Model;
 
+/**
+ * MetodePembayaran Model
+ * Model ini digunakan untuk berinteraksi dengan tabel metode_pembayaran
+ * yang menyimpan daftar metode pembayaran milik masing-masing client
+ */
 class MetodePembayaran extends Model
 {
+     // Nama tabel di database yang diwakili oleh model ini
     protected $table            = 'metode_pembayaran';
-    protected $allowedFields    = ['nama', 'client_id'];
+    // Field yang diizinkan untuk operasi insert dan update
+    // Digunakan untuk mencegah mass assignment vulnerability
+    protected $allowedFields    = [
+        'nama',  // Nama metode pembayaran (contoh: Tunai, Transfer, QRIS)
+        'client_id'  // ID client pemilik metode pembayaran
+    ];
 
-    // Dates
+    
+    // Mengaktifkan fitur timestamps otomatis
+    // created_at dan updated_at akan diisi otomatis oleh CodeIgniter
     protected $useTimestamps = true;
+     // Nama kolom untuk waktu pembuatan data
     protected $createdField  = 'created_at';
+     // Nama kolom untuk waktu terakhir update data
     protected $updatedField  = 'updated_at';
 }
